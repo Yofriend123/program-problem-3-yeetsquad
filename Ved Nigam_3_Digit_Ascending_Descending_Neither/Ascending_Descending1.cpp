5632f54bd0cfca6d0a63d2743a90e71d8f830876
Yofriend123 <31861826+Yofriend123@users.noreply.github.com>
2017-10-12 12:22:13 -0500

    Assignment 4
    Modified the Assignment 3 project by including comments and a loop for 30 numbers. John Bui

--- Ved Nigam_3_Digit_Ascending_Descending_Neither/Ascending_Descending1.cpp
+++ Ved Nigam_3_Digit_Ascending_Descending_Neither/Ascending_Descending1.cpp
@@ -1,14 +1,26 @@
+/* Overall changes: 
+Added date
+Comments on printing ascending, descending, and neither to clarify what happens
+Altered the ints to decrease the amount of lines
+Comments on cin and pause to clarify what happens
+Added comments for what is included in the libraries
+*/
+
 /*
-Ved Nigam-Period 3-Computer Science 1
+Ved Nigam-Period 3-Computer Science 1 October 5, 2017
 In this assignment, we will split a 3 digit number into 3 different numbers with the same characters as the 3 digit number. We will then run a tets to see if the numbers are
 "Descending," "Ascending," or "Neither."
 */
+// Added date
 
 //Libraries
-#include <iostream>
-#include <conio.h>
+#include <iostream> // gives access to cin, cout, endl, <<, >>, boolalpha, noboolalpha 
+#include <conio.h> // gives access to _kbhit() and _getch() for pause()
+// Added comments
+
 //Namespaces
 using namespace std;
+
 //Functions
 void pause() {
 	cout << "Press Any Key to Continue . . .";
@@ -18,26 +30,35 @@ void pause() {
 }
 
 void main() {
-	int x;
-	int a;
-	int b;
-	int c;
+	for (int i = 0; i < 30; i++) {
+		int x;
 
-	cout << "Enter a 3 Digit Number . . ." << endl;
-	cin >> x;
-	c = x % 10;
-	b = x / 10 % 10;
-	a = x / 100;
+		cout << "Enter a 3 Digit Number . . ." << endl;
+		cin >> x; // 'x' is the variable that stores the three digit number that the user types in.
+		// Added comments 
 
-	if (a > b && b > c) {
-		cout << "Descending" << endl;
+		int c = x % 10; // Isolates the digit in the 1's place
+		int b = x / 10 % 10; // Isolates the digit in the 10th's place
+		int a = x / 100; // Isolates the digit in the 100th's place
+		// Altered : Placed int c, int b, and int a in one area
+
+		if (a > b && b > c) {
+			cout << "Descending" << endl;
+		} // If the digits of 'x' are presented in a descending order from left to right, print 'descending'.
+		// Added comment for what it does
+		else if (a < b && b < c) {
+			cout << "Ascending" << endl;
+		} // If the digits of 'x' are presented in a ascending order from left to right, print 'ascending'.
+		// Added comment for what it does
+		else {
+			cout << "Neither" << endl;
+		} // If the digits of 'x' aren't present in either a descending or ascending order from left to right, print 'neither'
+		// Added comment for what it does
+
+		pause(); // Ends function, prints "Press any key to continue..."
+		// Added comment for what it does
 	}
-	else if (a < b && b < c) {
-		cout << "Ascending" << endl;
-	}
-	else {
-		cout << "Neither" << endl;
-	}
-	pause();
 }
+
+
 
